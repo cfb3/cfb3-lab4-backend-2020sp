114f7ed46292e321a66892efb6ee1a4a68690050
Charles Bryan <cfb3@uw.edu>
2020-04-23 13:40:46 -0700

    add hello get and post

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ const pool = new Pool({
     }
 })
 
+app.get("/hello", (request, response) => {
+    response.send({
+        message: "Hello, you sent a GET request"
+    })
+})
+
+app.post("/hello", (reqeust, response) => {
+    response.send({
+        message: "Hello, you sent a POST request"
+    })
+})
+
 
 
 
