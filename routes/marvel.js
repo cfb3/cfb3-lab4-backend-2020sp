4f6a2a028aecfa47349aeb79b17597b0c1674ea1
Charles Bryan <cfb3@uw.edu>
2020-06-24 13:27:28 -0700

    pushme: random and search endpoints

--- routes/marvel.js
+++ routes/marvel.js
@@ -87,4 +87,153 @@ router.get("/characters", (request, response) => {
 
 })
 
+/**
+ * @api {get} /marvel/characters Request a single random marvel character
+ * @apiName GetMarvelCharacters
+ * @apiGroup Marvel
+ * 
+ * @apiHeader {String} authorization JWT provided from Auth get
+ * 
+ * @apiDescription This end point is a pass through to the marvel.com api/ 
+ */ 
+router.get("/characters/random", (request, response) => {
+
+    let public_key = "29d6f0940bc494d38d918a29b2216239"
+    let private_key = "9c16c20e45ac1e82ec07dba169d5a3bf10eee27e"
+    let time_stamp = Date.now()
+
+    let hash = crypto.createHash('md5').update(time_stamp + private_key + public_key).digest("hex")
+    
+    let id = Math.floor(Math.random() * 1493);
+
+    let params = `?ts=${time_stamp}&apikey=${public_key}&hash=${hash}&offset=${id}&limit=1`
+
+    const options = {
+        hostname: 'gateway.marvel.com',
+        port: 443,
+        path: '/v1/public/characters' + params,
+        method: 'GET'
+    }
+
+    const req = https.request(options, res => {
+        
+        // console.log(`statusCode: ${res.statusCode}`)
+
+        let data = ""
+
+        res.on('data', d => {
+            data += d
+            // process.stdout.write(d)
+            // response.send("done")
+        })
+
+        res.on('end', () => {
+            // console.log(res)
+            console.log(data)
+
+            let results = JSON.parse(data).data.results
+
+            let characters = []
+            results.forEach(element => {
+                let character = {
+                    id:element.id,
+                    name:element.name,
+                    description:element.description,
+                    thumbnail:element.thumbnail
+                }
+                characters.push(character)
+            });
+
+            // console.log(characters);
+            response.send({
+                count:characters.length,
+                characters:characters
+            })
+        })
+    })
+
+    req.on('error', error => {
+        console.error(error)
+        response.send({message:"error"})
+    })
+    req.end()
+})
+
+/**
+ * @api {get} /marvel/characters Request a single random marvel character
+ * @apiName GetMarvelCharacters
+ * @apiGroup Marvel
+ * 
+ * @apiHeader {String} authorization JWT provided from Auth get
+ * 
+ * @apiDescription This end point is a pass through to the marvel.com api/ 
+ */ 
+router.get("/characters/search/:name", (request, response) => {
+
+    if (!request.params.name) {
+        response.status(400).send({
+            message: "Missing required information"
+        })
+    }
+
+    let public_key = "29d6f0940bc494d38d918a29b2216239"
+    let private_key = "9c16c20e45ac1e82ec07dba169d5a3bf10eee27e"
+    let time_stamp = Date.now()
+    let name = request.params.name
+
+    let hash = crypto.createHash('md5').update(time_stamp + private_key + public_key).digest("hex")
+    
+    let params = `?ts=${time_stamp}&apikey=${public_key}&hash=${hash}&nameStartsWith=${name}`
+
+    const options = {
+        hostname: 'gateway.marvel.com',
+        port: 443,
+        path: '/v1/public/characters' + params,
+        method: 'GET'
+    }
+
+    const req = https.request(options, res => {
+        
+        // console.log(`statusCode: ${res.statusCode}`)
+
+        let data = ""
+
+        res.on('data', d => {
+            data += d
+            // process.stdout.write(d)
+            // response.send("done")
+        })
+
+        res.on('end', () => {
+            // console.log(res)
+            console.log(data)
+
+            let results = JSON.parse(data).data.results
+
+            let characters = []
+            results.forEach(element => {
+                let character = {
+                    id:element.id,
+                    name:element.name,
+                    description:element.description,
+                    thumbnail:element.thumbnail
+                }
+                characters.push(character)
+            });
+
+            // console.log(characters);
+            response.send({
+                count:characters.length,
+                characters:characters
+            })
+        })
+    })
+
+    req.on('error', error => {
+        console.error(error)
+        response.send({message:"error"})
+    })
+    req.end()
+})
+
 module.exports = router
